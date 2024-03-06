c09f262fedd78e2c6ea9017890c786b70f17d423
BryanRO-SpTech <bryan.rocha@sptech.school>
2024-03-06 12:05:34 -0300

    Intervalo

---
+++ main.js
@@ -0,0 +1,15 @@
+const eco = document.getElementById("economia");
+const preju = document.getElementById("prejuizo")
+
+document.documentElement.style = "--tamanhoEconomia: 60%; --tamanhoPrejuizo: 40%; --tamanho-fonte-grafico: 1em;";
+document.documentElement.style = "--tamanhoEconomia: 60%; --tamanhoPrejuizo: 40%; --tamanho-fonte-grafico: 1em;";
+document.documentElement.style = "--tamanhoEconomia: 60%; --tamanhoPrejuizo: 40%; --tamanho-fonte-grafico: 1em;";
+eco.style.animation = "graficoEconomia 2s linear";
+preju.style.animation = "graficoPrejuizo 2s linear";
+
+
+function capturaDeDados() {
+    const modeloAviao = input_aviao;
+    const qtdManutencoes = input_qtd;
+    const tempoParado = input_tempo;
+}
